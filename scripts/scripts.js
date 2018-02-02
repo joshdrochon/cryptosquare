8d8713553acf74dd418d233032015c8a8c1000bc
Josh Rochon <joshdrochon@gmail.com>
2018-02-02 14:26:55 -0800

    pull th text and push into array

---
+++ scripts/scripts.js
@@ -0,0 +1,36 @@
+//business logic
+
+
+
+
+
+//ui logic
+
+$(document).ready(function(){
+  $("#decoder").click(function(event){
+    event.preventDefault();
+
+    function decryptor(){
+
+      var tableLetters = [];
+
+      $("table#cryptosquare tr").each(function(){
+        var thisTableRow = [];
+        var tableHeader = $(this).find('th');
+
+        if(tableHeader.length > 0){
+          tableHeader.each(function(){
+            thisTableRow.push($(this).text());
+          });
+            tableLetters.push(thisTableRow);
+          }//end if statment
+      });
+
+      console.log(tableLetters);
+
+    }//end decryptor
+
+    decryptor();
+
+  });
+});
